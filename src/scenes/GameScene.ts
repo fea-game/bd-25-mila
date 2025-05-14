bce78fcd3238d99cba3147e02d94eaa79887e940
Tobias Belch <tobias.belch@docmorris.de>
2025-05-14 17:15:18 +0200

    Create all character animations

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,6 +1,8 @@
 import Phaser from "phaser";
 import { SceneKey } from "../common/constants";
 
+const animations = ["walk-down", "walk-left", "walk-right", "walk-up"];
+
 export default class GameScene extends Phaser.Scene {
   character: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
@@ -15,18 +17,17 @@ export default class GameScene extends Phaser.Scene {
         this.cameras.main.height / 2,
         "character"
       )
-      .play("walk-down");
+      .play(animations[0]);
 
-    /*this.time.addEvent({
+    this.time.addEvent({
       delay: 3000,
       callback: this.changeAnimation,
       callbackScope: this,
-      loop: true
-    });*/
+      loop: true,
+    });
   }
 
   changeAnimation() {
-    const animations = ["walk-down", "walk-left", "walk-right", "walk-up"];
     const currentAnimation = this.character.anims.currentAnim;
     if (currentAnimation) {
       const nextAnimation =
