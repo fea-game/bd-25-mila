9a2c234c077d412b2cbcc88e036fd03b0fe861b8
Tobias Belch <tobias.belch@docmorris.de>
2025-05-16 10:49:28 +0200

    WIP: Create animations from merged file

--- src/scenes/preload-scene.ts
+++ src/scenes/preload-scene.ts
@@ -10,11 +10,38 @@ export default class PreloadScene extends Phaser.Scene {
   preload() {
     this.load.pack("main", "assets/data/assets.json");
     for (const [key, { url }] of Object.entries(Animation)) {
-      this.load.animation(key, url);
+      //this.load.animation(key, url);
     }
   }
 
   create() {
+    this.createAnimations();
+
     this.scene.start(SceneKey.Game);
+  }
+
+  private createAnimations() {
+    this.anims.createFromAseprite("character");
+
+    /*
+    this.anims.create({
+      key: "player-idle-down",
+      frames: [{ key: "character", frame: "player_idle_down_0" }],
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "player-walk-down",
+      frames: [
+        { key: "character", frame: "player_walk_down_0" },
+        { key: "character", frame: "player_walk_down_1" },
+        { key: "character", frame: "player_walk_down_0" },
+        { key: "character", frame: "player_walk_down_2" },
+      ],
+      frameRate: 6,
+      repeat: -1,
+    });
+    */
   }
 }
