043c99d63d273bd0a0965c3243310eefe3ef2e63
tobias-at-dm <tobias-at-dm@users.noreply.github.com>
2025-06-15 16:28:17 +0000

    deploy: 58fb838155a5a583b6236b39fe2b723de084aa56

---
+++ assets/tilesets/interior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="interior" tilewidth="48" tileheight="48" tilecount="200" columns="20">
+ <image source="../images/tilesets/interior.png" width="960" height="480"/>
+</tileset>
