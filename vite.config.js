58fb838155a5a583b6236b39fe2b723de084aa56
Tobias Belch <tobias.belch@docmorris.de>
2025-06-15 18:27:53 +0200

    Add deployment to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig(({ command }) => ({
+  base: "/bd-25-mila/",
   server: {
     port: 4000,
   },
